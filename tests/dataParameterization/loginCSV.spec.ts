8b0c26a5cd098d67bc4cdd9f5b536cee39b88fef
Raghul311 <raghulkumar311@gmail.com>
2025-11-29 01:07:38 +0530

    third commit

--- tests/dataParameterization/loginCSV.spec.ts
+++ tests/dataParameterization/loginCSV.spec.ts
@@ -2,6 +2,7 @@ import {expect, test} from"@playwright/test";
 import {parse} from "csv-parse/sync"
 import fs from "fs"
 
+test.describe.serial("test",async()=>{
 let records: any[]= parse(fs.readFileSync(`data/login.csv`),{columns:true,skip_empty_lines:true})
 for(let data of records){
 test(`Leaftap login via CSV ${data.tcID}`,async({page})=>{
@@ -10,4 +11,4 @@ test(`Leaftap login via CSV ${data.tcID}`,async({page})=>{
     await page.getByRole("textbox",{name:"Password"}).fill(data.password as string);
     await page.click(".decorativeSubmit");
     await page.waitForTimeout(5000);
-})}
+})}})
